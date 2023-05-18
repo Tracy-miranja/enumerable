07233e5c2efada85031a9aff58791eefb3499819
Tracy-miranja <barazatracy44@gmail.com>
2023-05-18 22:39:27 +0300

    Add github workflow

--- my_list.rb
+++ my_list.rb
@@ -12,7 +12,7 @@ class MyList
     end
   end
 
-  list = MyList.new(1, 2, 3, 4)
+list = MyList.new(1, 2, 3, 4)
 
 puts list.all? { |e| e < 5 }   # Output: true
 puts list.all? { |e| e > 5 }   # Output: false
