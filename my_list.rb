28377a738b698e5b1f259239ff711b1120f50d90
Tracy-miranja <barazatracy44@gmail.com>
2023-05-19 00:01:24 +0300

    Add the gemfile

--- my_list.rb
+++ my_list.rb
@@ -1,23 +1,23 @@
 require_relative 'my_enumerable'
 
 class MyList
-    include MyEnumerable
-  
-    def initialize(*items)
-      @list = items
-    end
-  
-    def each(&block)
-      @list.each(&block)
-    end
+  include MyEnumerable
+
+  def initialize(*items)
+    @list = items
   end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
 
 list = MyList.new(1, 2, 3, 4)
 
-puts list.all? { |e| e < 5 }   # Output: true
-puts list.all? { |e| e > 5 }   # Output: false
+puts(list.all? { |e| e < 5 }) # Output: true
+puts(list.all? { |e| e > 5 }) # Output: false
 
-puts list.any? { |e| e == 2 }  # Output: true
-puts list.any? { |e| e == 5 }  # Output: false
+puts(list.any? { |e| e == 2 }) # Output: true
+puts(list.any? { |e| e == 5 }) # Output: false
 
-puts list.filter { |e| e.even? }  # Output: [2, 4]
+puts(list.filter(&:even?)) # Output: [2, 4]
